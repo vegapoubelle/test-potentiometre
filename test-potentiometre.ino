1ea03a586446f1ca59a12f7b6964a4acc34fa559
Philippe Loctaux <loctauxphilippe@gmail.com>
2018-01-12 11:45:01 +0100

    pin to var

--- test-potentiometre.ino
+++ test-potentiometre.ino
@@ -1,13 +1,15 @@
 /* http://romain-astouric.com/arduino-1-etude-et-prise-en-main-dun-fader-motorise/ */
 
+int pin = 12;
+
 void setup() {
   Serial.begin(9600);
-  pinMode(2, INPUT);
+  pinMode(pin, INPUT);
 }
 
 void loop() {
   int valeur_recup = 0;
-  valeur_recup = analogRead(2);
+  valeur_recup = analogRead(pin);
   Serial.print("la position du curseur (aka line track) est de: ");
   Serial.println(valeur_recup);
 }
