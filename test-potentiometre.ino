78330cf84d64a68cd9cf0557f8b3a90cd525fc23
darkbenjo <bj.millot24@gmail.com>
2018-01-18 11:21:20 +0100

    Ajout cm
    Biere

--- test-potentiometre.ino
+++ test-potentiometre.ino
@@ -1,6 +1,7 @@
 /* http://romain-astouric.com/arduino-1-etude-et-prise-en-main-dun-fader-motorise/ */
 
 int pin = 12;
+float cm = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -10,8 +11,13 @@ void setup() {
 void loop() {
   int valeur_recup = 0;
   valeur_recup = analogRead(pin);
+  cm = (valeur_recup*9.3/4095);
   Serial.print("la position du curseur (aka line track) est de: ");
   Serial.println(valeur_recup);
+  Serial.print("Distance en cm : ");
+  Serial.println(cm);
+  Serial.println();
+  delay(500);
 }
 
 /* vim: set tabstop=2: */
