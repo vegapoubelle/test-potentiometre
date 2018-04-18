9297d5ca970fb5fb2ee90a17d745106309c0789d
Yann Le Guehennec <ylgdu56@gmail.com>
2018-04-18 18:20:14 +0200

    ok

--- test-potentiometre.ino
+++ test-potentiometre.ino
@@ -1,17 +1,15 @@
 /* http://romain-astouric.com/arduino-1-etude-et-prise-en-main-dun-fader-motorise/ */
 
-int pin = 5;
 float cm = 0;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(pin, INPUT);
 }
 
 void loop() {
   int valeur_recup = 0;
-  valeur_recup = analogRead(pin);
-  cm = (valeur_recup*9.3/4095);
+  valeur_recup = analogRead(A5);
+  //cm = (valeur_recup*9.3/4095);
   Serial.print("la position du curseur (aka line track) est de: ");
   Serial.println(valeur_recup);
   Serial.print("Distance en cm : ");
